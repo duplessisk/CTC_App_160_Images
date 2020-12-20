2099349cc62bd7922886c01db10cbfd65edaa339
duplessisk <klduplessis@gmail.com>
2020-12-20 08:23:09 -0800

    added submit button

--- image_test.js
+++ image_test.js
@@ -1,11 +1,17 @@
-
 // create flexbox-container-new-row div html elements
-for (var i = 1; i < 3; i++) {
+for (var i = 0; i < 3; i++) {
     var newDiv = document.createElement('div');
     document.body.appendChild(newDiv);
     newDiv.classList.add("flexbox-container-new-row");
     newDiv.innerHTML = "<div class='flexbox-container-new-row'><div class='checkbox-container'><div><input type='checkbox' class='yescheckboxes' id="+i+"> <label for='yes'>yes</label></div><input type='checkbox' class = 'nocheckboxes' id="+i+"> <label for='yes'>no</label></div><div class= 'checkbox-container'><img src='images/cell.jpg' id='cell'></div></div>";
 }
+
+// create button
+var submitButton = document.createElement('button');
+var submitButtonText = document.createTextNode('Submit');
+submitButton.appendChild(submitButtonText);
+submitButton.classList.add("submitbutton");
+document.body.appendChild(submitButton);
 
 var numYesCheckBoxes = document.querySelectorAll(".yescheckboxes").length;
 var numNoCheckBoxes = document.querySelectorAll(".nocheckboxes").length;
