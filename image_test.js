51e784e04b2a7dff05cf6caf1c62431ea60fc8ec
duplessisk <klduplessis@gmail.com>
2020-12-19 18:15:39 -0800

    more efficient code to handle clickbox data storage

--- image_test.js
+++ image_test.js
@@ -1,52 +1,31 @@
-// for (var i = 0; i < 3; i++) {
-//     var element = document.getElementById("new-row");
-//     var clone = element.cloneNode(true);
-//     element.after(clone);
-// }
-var responses = new Array(4);
+var responses = new Array(6);
+
 for (var i = 0; i < responses.length; i++) {
-    responses[i] = null;
+    responses[i] = false;
     console.log(responses[i]);
 }
 
-const yesBox = document.querySelector('#yes');
-const noBox = document.querySelector('#no');
-var submitButton = document.querySelector(".submitbutton");
+var numYesCheckBoxes = document.querySelectorAll(".yescheckboxes").length;
+var numNoCheckBoxes = document.querySelectorAll(".nocheckboxes").length;
 
-for (var i = 0; i < document.querySelectorAll("yescheckboxes").length; i++) {
-    document.querySelectorAll("yescheckboxes")[i].addEventListener('change', function() {
-        alert("I got checked");
-        // if (this.id === "1") {
-        //     alert("first yes check box checked");
-        // } else if (this.id === "2") {
-        //     alert("second yes box checked")
-        // } else {
-        //     alert("third yes box checked");
-        // }
+for (var i = 0; i < numYesCheckBoxes; i++) {
+    document.querySelectorAll(".yescheckboxes")[i].addEventListener('change', function() {
+        if (this.checked) {
+            responses[2*this.id] = true;
+        } else {
+            responses[2*this.id] = false;
+        }
+        console.log(responses);
     });
 }
 
-// yesBox.addEventListener('change',function(e) {
-//     if (yesBox.checked) {
-//         responses[0] = true;
-//         console.log(responses[0]);
-//     }
-// });
-
-// noBox.addEventListener('change',function(e) {
-//     if (noBox.checked) {
-//         responses[0] = false;
-//         console.log(responses[0]);
-//     }
-// });
-
-// submitButton.addEventListener("click",function(e) {
-//     var numCorrect = 0;
-//     for (var i = 0; i < responses.length ; i++) {
-//         if (responses[i]) {
-//             numCorrect++;
-//         }
-//     }
-//     var score = numCorrect/1;
-//     alert("score: " + score);
-// });
+for (var i = 0; i < numNoCheckBoxes; i++) {
+    document.querySelectorAll(".nocheckboxes")[i].addEventListener('change', function() {
+        if (this.checked) {
+            responses[2*this.id + 1] = true;
+        } else {
+            responses[2*this.id + 1] = false;
+        }
+        console.log(responses);
+    });
+}
