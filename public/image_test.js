8d11dc8aec6a44779144845f8c0271da319b1db1
duplessisk <klduplessis@gmail.com>
2020-12-23 07:30:53 -0800

    changed image path

--- public/image_test.js
+++ public/image_test.js
@@ -14,7 +14,7 @@
 var div1 = document.createElement('div');
 document.querySelector("#form").appendChild(div1);
 div1.classList.add("flexbox-container-new-row");
-div1.innerHTML = "<div class='flexbox-container-new-row'><div class='checkbox-container'><div><input type='checkbox' name='yes0' class='yescheckboxes' id='0'> <label for='yes'>CTC</label></div><input type='checkbox' name='no0' class = 'nocheckboxes' id='0'> <label for='yes'>not-CTC</label></div><div class= 'checkbox-container'><img src='public/images/ctcCell.jpg' id='cell'></div></div>";
+div1.innerHTML = "<div class='flexbox-container-new-row'><div class='checkbox-container'><div><input type='checkbox' name='yes0' class='yescheckboxes' id='0'> <label for='yes'>CTC</label></div><input type='checkbox' name='no0' class = 'nocheckboxes' id='0'> <label for='yes'>not-CTC</label></div><div class= 'checkbox-container'><img src='/static/ctcCell.jpg' id='cell'></div></div>";
 
 var responses = [false,false,false,false];
 var numYesCheckBoxes = document.querySelectorAll(".yescheckboxes").length;
