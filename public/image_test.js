643fc38e5faad083fa3d665249ed9294559309a5
duplessisk <klduplessis@gmail.com>
2020-12-23 06:53:54 -0800

    tweaking with images

--- public/image_test.js
+++ public/image_test.js
@@ -14,12 +14,8 @@
 var div1 = document.createElement('div');
 document.querySelector("#form").appendChild(div1);
 div1.classList.add("flexbox-container-new-row");
-div1.innerHTML = "<div class='flexbox-container-new-row'><div class='checkbox-container'><div><input type='checkbox' name='yes0' class='yescheckboxes' id='0'> <label for='yes'>CTC</label></div><input type='checkbox' name='no0' class = 'nocheckboxes' id='0'> <label for='yes'>not-CTC</label></div><div class= 'checkbox-container'><img src='/images/ctcCell.jpg' id='cell'></div></div>";
+div1.innerHTML = "<div class='flexbox-container-new-row'><div class='checkbox-container'><div><input type='checkbox' name='yes0' class='yescheckboxes' id='0'> <label for='yes'>CTC</label></div><input type='checkbox' name='no0' class = 'nocheckboxes' id='0'> <label for='yes'>not-CTC</label></div><div class= 'checkbox-container'><img src='/public/images/ctcCell.jpg' id='cell'></div></div>";
 
-var div2 = document.createElement('div');
-document.querySelector("#form").appendChild(div2);
-div2.classList.add("flexbox-container-new-row");
-div2.innerHTML = "<div class='flexbox-container-new-row'><div class='checkbox-container'><div><input type='checkbox' name='yes1' class='yescheckboxes' id='1'> <label for='yes'>CTC</label></div><input type='checkbox' name='no1' class = 'nocheckboxes' id='1'> <label for='yes'>not-CTC</label></div><div class= 'checkbox-container'><img src='/static/images/notCTCcell.jpg' id='cell'></div></div>";
 var responses = [false,false,false,false];
 var numYesCheckBoxes = document.querySelectorAll(".yescheckboxes").length;
 var numNoCheckBoxes = document.querySelectorAll(".nocheckboxes").length;
