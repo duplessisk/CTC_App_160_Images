b9fca8d9845b9c087a496edd4aa6f0b8039ae6da
Kyle Duplessis <klduplessis@gmail.com>
2020-12-26 18:16:52 -0800

    user can set answer key in seperate file

---
+++ answerKey.js
@@ -0,0 +1,4 @@
+ // image num :   1   2
+var answerKey = ['y','n'];
+
+exports.answerKey = answerKey;
--- server.js
+++ server.js
@@ -2,11 +2,8 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const readline = require('readline');
-// var lineByLine = require('n-readlines');
-var stream = require('stream');
 const bodyParser = require("body-parser");
-const LineByLine = require("n-readlines");
+const answerKey = require("./answerKey");
 
 // create express app
 const app = express();
@@ -22,61 +19,29 @@ app.get("/", function(request,response) {
 
 app.listen(process.env.PORT || 3000);
 
-var answerKey = [];
-
 app.post("/", function(request,response) {
     // var answerKey = [true,false,false,true];
     var answerKey = createAnswerKey();
     var userResponses = [];
     initUserResponses(request,userResponses);
     parseUserResponses(userResponses);
-    processWrongAnswers(answerKey,userResponses);
+    writeMissedImagePaths(answerKey,userResponses);
     response.sendFile(path.join(__dirname + '/results.html'));
 });
 
 function createAnswerKey() {
-    var instream = fs.createReadStream('./answerKey.txt');
-    var outstream = new stream;
-    var rl = readline.createInterface(instream, outstream);
-    
-    var arr = [];
-    
-    rl.on('line', function(line) {
-        console.log("answer: " + line);
-        if () {
-            arr.push(true);
-            arr.push(false);
-        } else if () {
-            arr.push(false);
-            arr.push(true);
-        }
-        console.log("answer equals y" + line == "y");
-        console.log("answer equals n" + line === "n");
-    });
-    
-    rl.on('close', function() {
-      // do something on finish here
-      console.log('arr', arr);
-    });
-    return [true,false,false,true];
-}
-
-function processWrongAnswers(answerKey,userResponses) {
-    var jsonString = "";
-    for (var i = 0; i < answerKey.length/2; i++) {
-        if (answerKey[2*i] != userResponses[2*i] || answerKey[2*i + 1] != userResponses[2*i + 1]) {
-            //write image path to JSON file
-            var wrongObject = {
-                imagePath: '/static/cell_images/cell' + String(i) + '.JPG'
-            }
-            jsonString += JSON.stringify(wrongObject);
+    var answerKey = answerKey.answerKey;
+    var answers = [];
+    for (var i = 0; i < answerKey.length; i++) {
+        if (answerKey[i] === "y") {
+            answers.push(true);
+            answers.push(false);
+        } else if (answerKey[i] === "n") {
+            answers.push(false);
+            answers.push(true);
         }
     }
-    fs.writeFile('./public/incorrect_image_paths.json',jsonString, function(error) {
-        if (error) {
-            console.log(error);
-        }
-    })
+    return answers;
 }
 
 function initUserResponses(request,userResponses) {
@@ -95,4 +60,22 @@ function parseUserResponses(userResponses) {
             userResponses[i] = true;
         }
     }
+}
+
+function writeMissedImagePaths(answerKey,userResponses) {
+    var jsonString = "";
+    for (var i = 0; i < answerKey.length/2; i++) {
+        if (answerKey[2*i] != userResponses[2*i] || answerKey[2*i + 1] != userResponses[2*i + 1]) {
+            //write image path to JSON file
+            var wrongObject = {
+                imagePath: '/static/cell_images/cell' + String(i) + '.JPG'
+            }
+            jsonString += JSON.stringify(wrongObject);
+        }
+    }
+    fs.writeFile('./public/incorrect_image_paths.json',jsonString, function(error) {
+        if (error) {
+            console.log(error);
+        }
+    })
 }
