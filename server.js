18189220ba6262c29621a4211c9eb0d6debe65e6
Kyle Duplessis <klduplessis@gmail.com>
2020-12-29 07:59:35 -0800

    cleaned up server.js

--- server.js
+++ server.js
@@ -21,11 +21,7 @@ app.get("/", function(request,response) {
 });
 
 app.post("/", function(request,response) {
-    var answerKey1 = createAnswerKey(answer_key_page_one);
-    var userResponses = [];
-    initUserResponses(request,userResponses);
-    recordUserResponses(userResponses);
-    getMissedImagePaths(answerKey1, userResponses, 0, answerKey1.length/2 - 1);
+    driveApp(answer_key_page_one,request,0);
     response.redirect('/page_two');
 });
 
@@ -34,15 +30,7 @@ app.get("/page_two", function(request,response) {
 });
 
 app.post("/page_two", function(request,response) {
-    var answerKey2 = createAnswerKey(answer_key_page_two);
-    var userResponses = [];
-    initUserResponses(request,userResponses);
-    recordUserResponses(userResponses);
-    getMissedImagePaths(answerKey2, userResponses, answerKey2.length/2, answerKey2.length - 1);
-    console.log("page2: ");
-    console.log("userResponses: " + userResponses);
-    console.log("answerKey: " + answerKey2);
-    console.log("jsonString: " + jsonString);
+    driveApp(answer_key_page_two,request,5);
     writeMissedImagePaths();
     response.redirect('/results');
 });
@@ -53,49 +41,62 @@ app.get("/results", function(request,response) {
 
 app.listen(process.env.PORT || 3000);
 
+function driveApp(answerKeyPage,request,firstCellImageNumber) {
+    var answerKey = createAnswerKey(answerKeyPage);
+    var userResponses = [];
+    var userResponses = initUserResponses(request);
+    recordUserResponses(userResponses);
+    getMissedImagePaths(answerKey, userResponses, firstCellImageNumber);
+}
+
 function createAnswerKey(answerKey) {
     var ansKey = answerKey.answerKey;
     var answers = [];
     for (var i = 0; i < ansKey.length; i++) {
         if (ansKey[i] === "y") {
-            answers.push(true);
-            answers.push(false);
+            ansKey[i] = true;
         } else if (ansKey[i] === "n") {
-            answers.push(false);
-            answers.push(true);
-        }
+            ansKey[i] = false;
+        } 
     }
     return answers;
 }
 
-function initUserResponses(request,userResponses) {
-    var names = [request.body.yes0,request.body.no0,
-                 request.body.yes1,request.body.no1,
-                 request.body.yes2,request.body.no2,
-                 request.body.yes3,request.body.no3,
-                 request.body.yes4,request.body.no4];
-    for (var i = 0; i < names.length; i++) {
-        userResponses[i] = String(names[i]);
-    }
-}
+function checkAnswerKey(answerKey) {
+    for (var i = 0; i < answerKey.length; i++) {
+        try {
 
-// convert user responses to booleans that can be compared against the answer key
-function recordUserResponses(userResponses) {
-    for (var i = 0; i < userResponses.length; i++) {
-        if (userResponses[i] == "undefined") {
-            userResponses[i] = false;
-        } else {
-            userResponses[i] = true;
+        } catch {
+            
         }
     }
 }
 
-function getMissedImagePaths(answerKey,userResponses,start,end) { 
-    for (var i = start; i <= end; i++) {
-        if (answerKey[2*(i-start)] != userResponses[2*(i-start)] || answerKey[2*(i-start) + 1] != userResponses[2*(i-start) + 1]) {
-            //write image path to JSON file
+function initUserResponses(request) {
+    return  [
+                request.body.yes0,
+                request.body.yes1,
+                request.body.yes2,
+                request.body.yes3,
+                request.body.yes4
+            ];
+}
+
+function recordUserResponses(userResponses) {
+    for (var i = 0; i < userResponses.length; i++) {
+        if (userResponses[i] == "on") {
+            userResponses[i] = true;
+        } else {
+            userResponses[i] = false;
+        }
+    }
+}
+
+function getMissedImagePaths(answerKey,userResponses,firstCellImageNumber) { 
+    for (var i = 0; i < 5; i++) {
+        if (answerKey[i] != userResponses[i]) {
             var wrongObject = {
-                imagePath: '/static/cell_answers/cell' + String(i) + 'answer.JPG'
+                imagePath: '/static/cell_answers/cell' + String(i + firstCellImageNumber) + 'answer.JPG'
             }
             jsonString += JSON.stringify(wrongObject);
         }
