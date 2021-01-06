1b3da1a29dca0215565a5ac2ff7462f2284c6c4a
duplessisk <klduplessis@gmail.com>
2021-01-06 07:11:39 -0800

    cleaned up variable names

--- public/create_results.js
+++ public/create_results.js
@@ -7,117 +7,111 @@ var cellTypes = ['A','A','A','A','A','A','A','A','A','A',
                 ];
 
 // create DIVs
-var buttonTypeA = document.createElement('div');
-buttonTypeA.className = "button_type";
-buttonTypeA.id = "buttonTypeA";
-document.body.appendChild(buttonTypeA);
-var typeA = document.createElement('div');
-typeA.id = "typeA";
-typeA.className = "types";
-document.body.appendChild(typeA);
+var typeAHeaderDiv = document.createElement('div');
+typeAHeaderDiv.id = "typeAHeaderDiv";
+document.body.appendChild(typeAHeaderDiv);
+var typeAResultsDiv = document.createElement('div');
+typeAResultsDiv.id = "typeAResultDiv";
+typeAResultsDiv.className = "types";
+document.body.appendChild(typeAResultsDiv);
 var typeALabel = document.createElement('p');
 typeALabel.innerHTML = "Type A Cell Results";
 typeALabel.className = "label-types";
-document.getElementById("buttonTypeA").appendChild(typeALabel);
+document.getElementById("typeAHeaderDiv").appendChild(typeALabel);
 
-var buttonTypeB = document.createElement('div');
-buttonTypeB.className = "button_type";
-buttonTypeB.id = "buttonTypeB";
-document.body.appendChild(buttonTypeB);
-var typeB = document.createElement('div');
-typeB.id = "typeB";
-typeB.className = "types";
-document.body.appendChild(typeB);
+var typeBHeaderDiv = document.createElement('div');
+typeBHeaderDiv.id = "typeBHeaderDiv";
+document.body.appendChild(typeBHeaderDiv);
+var typeBResultsDiv = document.createElement('div');
+typeBResultsDiv.id = "typeBResultDiv";
+typeBResultsDiv.className = "types";
+document.body.appendChild(typeBResultsDiv);
 var typeBLabel = document.createElement('p');
 typeBLabel.innerHTML = "Type B Cell Results";
 typeBLabel.className = "label-types";
-document.getElementById("buttonTypeB").appendChild(typeBLabel);
+document.getElementById("typeBHeaderDiv").appendChild(typeBLabel);
 
-var buttonTypeC = document.createElement('div');
-buttonTypeC.className = "button_type";
-buttonTypeC.id = "buttonTypeC";
-document.body.appendChild(buttonTypeC);
-var typeC = document.createElement('div');
-typeC.id = "typeC";
-typeC.className = "types";
-document.body.appendChild(typeC);
+var typeCHeaderDiv = document.createElement('div');
+typeCHeaderDiv.id = "typeCHeaderDiv";
+document.body.appendChild(typeCHeaderDiv);
+var typeCResultsDiv = document.createElement('div');
+typeCResultsDiv.id = "typeCResultDiv";
+typeCResultsDiv.className = "types";
+document.body.appendChild(typeCResultsDiv);
 var typeCLabel = document.createElement('p');
 typeCLabel.innerHTML = "Type C Cell Results";
 typeCLabel.className = "label-types";
-document.getElementById("buttonTypeC").appendChild(typeCLabel);
+document.getElementById("typeCHeaderDiv").appendChild(typeCLabel);
 
-var buttonTypeD = document.createElement('div');
-buttonTypeD.className = "button_type";
-buttonTypeD.id = "buttonTypeD";
-document.body.appendChild(buttonTypeD);
-var typeD = document.createElement('div');
-typeD.id = "typeD";
-typeD.className = "types";
-document.body.appendChild(typeD);
+var typeDHeaderDiv = document.createElement('div');
+typeDHeaderDiv.id = "typeDHeaderDiv";
+document.body.appendChild(typeDHeaderDiv);
+var typeDResultsDiv = document.createElement('div');
+typeDResultsDiv.id = "typeDResultDiv";
+typeDResultsDiv.className = "types";
+document.body.appendChild(typeDResultsDiv);
 var typeDLabel = document.createElement('p');
 typeDLabel.innerHTML = "Type D Cell Results";
 typeDLabel.className = "label-types";
-document.getElementById("buttonTypeD").appendChild(typeDLabel);
+document.getElementById("typeDHeaderDiv").appendChild(typeDLabel);
 
-var buttonTypeE = document.createElement('div');
-buttonTypeE.className = "button_type";
-buttonTypeE.id = "buttonTypeE";
-document.body.appendChild(buttonTypeE);
-var typeE = document.createElement('div');
-typeE.id = "typeE";
-typeE.className = "types";
-document.body.appendChild(typeE);
+var typeEHeaderDiv = document.createElement('div');
+typeEHeaderDiv.id = "typeEHeaderDiv";
+document.body.appendChild(typeEHeaderDiv);
+var typeEResultsDiv = document.createElement('div');
+typeEResultsDiv.id = "typeEResultDiv";
+typeEResultsDiv.className = "types";
+document.body.appendChild(typeEResultsDiv);
 var typeELabel = document.createElement('p');
 typeELabel.innerHTML = "Type E Cell Results";
 typeELabel.className = "label-types";
-document.getElementById("buttonTypeE").appendChild(typeELabel);
+document.getElementById("typeEHeaderDiv").appendChild(typeELabel);
 
 // create buttons
-var showTypeAButton = document.createElement('button');
-showTypeAButton.innerHTML = "Show";
-showTypeAButton.id = "showTypeAButton";
-showTypeAButton.className = "show-type-button";
-document.querySelector("#buttonTypeA").appendChild(showTypeAButton);
+var typeAButton = document.createElement('button');
+typeAButton.innerHTML = "Show";
+typeAButton.id = "typeAButton";
+typeAButton.className = "show-type-button";
+document.querySelector("#typeAHeaderDiv").appendChild(typeAButton);
 
-var showTypeBButton = document.createElement('button');
-showTypeBButton.innerHTML = "Show";
-showTypeBButton.id = "showTypeBButton";
-showTypeBButton.className = "show-type-button";
-document.querySelector("#buttonTypeB").appendChild(showTypeBButton);
+var typeBButton = document.createElement('button');
+typeBButton.innerHTML = "Show";
+typeBButton.id = "typeBButton";
+typeBButton.className = "show-type-button";
+document.querySelector("#typeBHeaderDiv").appendChild(typeBButton);
 
-var showTypeCButton = document.createElement('button');
-showTypeCButton.innerHTML = "Show";
-showTypeCButton.id = "showTypeCButton";
-showTypeCButton.className = "show-type-button";
-document.querySelector("#buttonTypeC").appendChild(showTypeCButton);
+var typeCButton = document.createElement('button');
+typeCButton.innerHTML = "Show";
+typeCButton.id = "typeCButton";
+typeCButton.className = "show-type-button";
+document.querySelector("#typeCHeaderDiv").appendChild(typeCButton);
 
-var showTypeDButton = document.createElement('button');
-showTypeDButton.innerHTML = "Show";
-showTypeDButton.id = "showTypeDButton";
-showTypeDButton.className = "show-type-button";
-document.querySelector("#buttonTypeD").appendChild(showTypeDButton);
+var typeDButton = document.createElement('button');
+typeDButton.innerHTML = "Show";
+typeDButton.id = "typeDButton";
+typeDButton.className = "show-type-button";
+document.querySelector("#typeDHeaderDiv").appendChild(typeDButton);
 
-var showTypeEButton = document.createElement('button');
-showTypeEButton.innerHTML = "Show";
-showTypeEButton.id = "showTypeEButton";
-showTypeEButton.className = "show-type-button";
-document.querySelector("#buttonTypeE").appendChild(showTypeEButton);
+var typeEButton = document.createElement('button');
+typeEButton.innerHTML = "Show";
+typeEButton.id = "typeEButton";
+typeEButton.className = "show-type-button";
+document.querySelector("#typeEHeaderDiv").appendChild(typeEButton);
 
 
 //make buttons dynamic
 var buttonsClickNumMap = new Map([['A', 0], ['B', 0], ['C', 0], ['D', 0], ['E', 0]]);
 
-var buttonTypeArray = [showTypeAButton.id,showTypeBButton.id,showTypeCButton.id,showTypeDButton.id,showTypeEButton.id];
-
-for (var i = 0; i < buttonTypeArray.length; i++) {
+for (var i = 0; i < 5; i++) {
     document.querySelectorAll(".show-type-button")[i].addEventListener('click', function() {
-        var s = this.id.charAt(8);
+        var s = this.id.charAt(4);
+        console.log("clicked button ID: " + s);
         if (buttonsClickNumMap.get(s)%2 == 0) {
             getBlock(s,'show');
-            document.getElementById("showType"+s+"Button").innerHTML = "Hide";
+            document.getElementById("type"+s+"Button").innerHTML = "Hide";
         } else {
             getBlock(s,'hide');
-            document.getElementById("showType"+s+"Button").innerHTML = "Show";
+            document.getElementById("type"+s+"Button").innerHTML = "Show";
         }
         buttonsClickNumMap.set(s,buttonsClickNumMap.get(s) + 1);
     });
@@ -165,7 +159,6 @@ function buildDoc(arr,globalCellType,showOrHide) {
     }
 }
 
-
 // functions to process data from JSON file
 function getMissedImageSrc(missedImagePath) {
     missedImagePath = missedImagePath.substring(1);
@@ -186,13 +179,11 @@ function placeInCorrectCategory(imageNum,newImg,globalCellType,showOrHide) {
         messageDiv.id = "messageDiv";
         messageDiv.innerHTML = "You got image  " + imageNum + " incorrect";
         if (showOrHide == "show") {
-            document.querySelector("#type"+localCellType+"").appendChild(messageDiv);
-            document.querySelector("#type"+localCellType+"").appendChild(newImg);
+            document.querySelector("#type"+localCellType+"ResultDiv").appendChild(messageDiv);
+            document.querySelector("#type"+localCellType+"ResultDiv").appendChild(newImg);
         }
         if (showOrHide == "hide") {
-            document.querySelector("#type"+localCellType+"").innerHTML = '';
+            document.querySelector("#type"+localCellType+"ResultDiv").innerHTML = '';
         }
     }
-} 
-
-
+}
