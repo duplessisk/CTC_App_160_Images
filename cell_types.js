0be5e2ff06421342520fb011b13c1cec6b8fa771
duplessisk <klduplessis@gmail.com>
2021-01-15 06:38:35 -0800

    m

--- cell_types.js
+++ cell_types.js
@@ -20,16 +20,16 @@
 //     answerKeys[Math.floor(i/10)].push(cellInfo[i][2]);
 // }
 
-answerKeys = [ ["y","y","y","y","y","y","y","y","y","y"], 
+answerKeys = [["y","y","y","y","y","y","y","y","y","y"], 
                ["y","y","y","y","y","y","y","y","y","y"],
                ["y","y","y","y","y","y","y","y","y","y"],
                ["y","y","y","y","y","y","y","y","y","y"],
                ["y","y","y","y","y","y","y","y","y","y"]];
-cellTypes = [["A","A","A","A","A","A","A","A","A","A"],
-             ["B","B","B","B","B","B","B","B","B","B"],
-             ["C","C","C","C","C","C","C","C","C","C"],
-             ["D","D","D","D","D","D","D","D","D","D"],
-             ["E","E","E","E","E","E","E","E","E","E"],
+cellTypes = ["A","A","A","A","A","A","A","A","A","A",
+             "B","B","B","B","B","B","B","B","B","B",
+             "C","C","C","C","C","C","C","C","C","C",
+             "D","D","D","D","D","D","D","D","D","D",
+             "E","E","E","E","E","E","E","E","E","E",
             ]
 exports.answerKeys = answerKeys;
 exports.cellTypes = cellTypes;
