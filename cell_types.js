cf2ef4ad2c68d86b9fa559aa4ce5015b8e695db8
duplessisk <klduplessis@gmail.com>
2021-01-15 13:06:13 -0800

    things

--- cell_types.js
+++ cell_types.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 var cellInfo = [];
 // var fileContents = fs.readFileSync(path.join(__dirname + '/cell_information.csv'));
 var fileContents = fs.readFileSync(__dirname + '/cell_information.csv');
+console.log(fileContents);
 
 // var fileContents = fs.readFileSync('./cell_information.csv');
 
@@ -12,7 +13,6 @@ for (var i = 0; i < rows.length; i++) {
 }
 
 cellInfo = cellInfo.splice(1,cellInfo.length - 2);
-console.log(cellInfo);
 cellTypes = [];
 answerKeys = [ [], [], [], [], [] ];
 //     page:   1    2    3    4    5
@@ -21,10 +21,6 @@ for (var i = 0; i < cellInfo.length; i++) {
     cellTypes.push(cellInfo[i][1]);
     answerKeys[Math.floor(i/10)].push(cellInfo[i][2]);
 }
-
-
-console.log(cellTypes);
-console.log(answerKeys);
 
 // answerKeys = [["y","y","y","y","y","y","y","y","y","y"], 
 //                ["y","y","y","y","y","y","y","y","y","y"],
