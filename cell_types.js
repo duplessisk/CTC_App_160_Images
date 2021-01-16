eead85aacb0862ec30f95fed42200c46a0e16a90
duplessisk <klduplessis@gmail.com>
2021-01-15 22:41:25 -0800

    m

--- cell_types.js
+++ cell_types.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 var cellInfo = [];
 var fileContents = fs.readFileSync(__dirname + '/cell_information.csv');
-console.log(fileContents);
+console.log(fileContents.toString());
 var rows = fileContents.toString('utf-8').split('\r\n');
 
 console.log(rows.length);
