12a62fec3a7aba624f3db822df7a879243a6f30d
duplessisk <klduplessis@gmail.com>
2021-01-15 18:26:10 -0800

    l

--- cell_types.js
+++ cell_types.js
@@ -1,42 +1,27 @@
 const fs = require('fs');
-// var fileContents = fs.readFileSync(path.join(__dirname + '/cell_information.csv'));
-var fileContents = fs.readFileSync(__dirname + '/cell_information.csv');
-
-// var rows = fileContents.toString().split('\r');
-var rows = fileContents.toString().split(',');
-console.log("rows: ");
-console.log(rows);
 
 var cellInfo = [];
+var fileContents = fs.readFileSync('./cell_information.CSV');
+var rows = fileContents.toString().split('\r\n');
 
 for (var i = 0; i < rows.length; i++) {
-    cellInfo.push(rows[i].toString().split(','));
+	cellInfo.push(rows[i].toString().split(','));
 }
 
-cellInfo.splice(3,49);
-console.log("cellInfo");
-console.log(cellInfo);
-
 cellInfo = cellInfo.splice(1,cellInfo.length - 2);
+
 cellTypes = [];
 answerKeys = [ [], [], [], [], [] ];
-//     page:   1    2    3    4    5
+
+//      page:   1    2    3    4    5
 answerKeys = [ [] , [] , [] , [] , [] ];
 for (var i = 0; i < cellInfo.length; i++) {
     cellTypes.push(cellInfo[i][1]);
     answerKeys[Math.floor(i/10)].push(cellInfo[i][2]);
 }
 
-// answerKeys = [["y","y","y","y","y","y","y","y","y","y"], 
-//                ["y","y","y","y","y","y","y","y","y","y"],
-//                ["y","y","y","y","y","y","y","y","y","y"],
-//                ["y","y","y","y","y","y","y","y","y","y"],
-//                ["y","y","y","y","y","y","y","y","y","y"]];
-// cellTypes = ["A","A","A","A","A","A","A","A","A","A",
-//              "B","B","B","B","B","B","B","B","B","B",
-//              "C","C","C","C","C","C","C","C","C","C",
-//              "D","D","D","D","D","D","D","D","D","D",
-//              "E","E","E","E","E","E","E","E","E","E",
-//             ]
+console.log(cellTypes);
+console.log(answerKeys);
+
 exports.answerKeys = answerKeys;
 exports.cellTypes = cellTypes;
