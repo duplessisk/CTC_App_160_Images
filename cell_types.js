4c575dda63c0b5272c61dcdc7020aa8ed65d1c6d
duplessisk <klduplessis@gmail.com>
2021-01-15 18:29:57 -0800

    m

--- cell_types.js
+++ cell_types.js
@@ -4,6 +4,9 @@ var cellInfo = [];
 var fileContents = fs.readFileSync(__dirname + '/cell_information.csv');
 var rows = fileContents.toString().split('\r\n');
 
+console.log("rows");
+console.log(rows);
+
 for (var i = 0; i < rows.length; i++) {
 	cellInfo.push(rows[i].toString().split(','));
 }
