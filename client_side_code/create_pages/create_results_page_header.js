dbd2ce0b3798037e966d36e923b534110d42afe3
duplessisk <klduplessis@gmail.com>
2021-01-28 09:23:31 -0800

    modified behavior of review_page header

--- client_side_code/create_pages/create_results_page_header.js
+++ client_side_code/create_pages/create_results_page_header.js
@@ -1,12 +1,10 @@
 window.addEventListener('scroll', function() {
     console.log(document.documentElement.scrollTop);
-    if (document.documentElement.scrollTop > 0) {
-        // document.getElementById("pageHeaderDiv").classList.remove('small');
+    if (document.documentElement.scrollTop > 5) {
         console.log("add small");
         document.getElementById("pageHeaderDiv").classList.add('small');
     } else {
         console.log("remove small");
         document.getElementById("pageHeaderDiv").classList.remove('small');
-        // document.getElementById("pageHeaderDiv").classList.add('small');
     }
 });
