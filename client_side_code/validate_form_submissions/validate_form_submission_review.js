b60e3137adaa187d6968bcca4d953c295265de73
duplessisk <klduplessis@gmail.com>
2021-01-30 11:36:04 -0800

    review page buttons now work

--- client_side_code/validate_form_submissions/validate_form_submission_review.js
+++ client_side_code/validate_form_submissions/validate_form_submission_review.js
@@ -29,11 +29,6 @@ var pageFourHasNull = localStorage.getItem('pageFourHasNull');
 var pageFiveHasNull = localStorage.getItem('pageFiveHasNull');
 
 var initBuffer = document.createElement('div');
-var buffer1 = document.createElement('div');
-var buffer2 = document.createElement('div');
-var buffer3 = document.createElement('div');
-var buffer4 = document.createElement('div');
-
 
 if (pageOneHasNull == "true" || pageTwoHasNull == "true" ||
     pageThreeHasNull == "true" || pageFourHasNull == "true" || pageFiveHasNull == "true") {
@@ -41,46 +36,65 @@ if (pageOneHasNull == "true" || pageTwoHasNull == "true" ||
     skippedPages.innerHTML = "You didn't answer questions on the following page(s): ";
     skippedPages.className = "review-page-messages";
     document.querySelector("#reviewMessageResultsDiv").appendChild(skippedPages);
-    document.querySelector('#reviewMessageResultsDiv').appendChild(initBuffer);
 }
 
 if (pageOneHasNull == "true") {
-    var pageOneNullMessage = document.createElement('a');
-    pageOneNullMessage.href = "./page_1";
-    pageOneNullMessage.innerHTML = "Page One";
-    document.querySelector("#reviewMessagePagesDiv").appendChild(pageOneNullMessage);
-    document.querySelector('#reviewMessagePagesDiv').appendChild(buffer1);
+    var pageOneNullbtn = document.createElement('button');
+    pageOneNullbtn.innerHTML = "Page One";
+    pageOneNullbtn.className = "null-btns";
+    pageOneNullbtn.name = "btn";
+    pageOneNullbtn.value = "pageOneNull";
+    pageOneNullbtn.type = "submit";
+    document.querySelector("#nullPagesDiv").appendChild(pageOneNullbtn);
 }
 
 if (pageTwoHasNull == "true") {
-    var pageTwoNullMessage = document.createElement('a');
-    pageTwoNullMessage.href = "./page_2";
-    pageTwoNullMessage.innerHTML = "Page Two";
-    document.querySelector("#reviewMessagePagesDiv").appendChild(pageTwoNullMessage);
-    document.querySelector('#reviewMessagePagesDiv').appendChild(buffer2);
+    var pageTwoNullbtn = document.createElement('button');
+    pageTwoNullbtn.innerHTML = "Page Two";
+    pageTwoNullbtn.className = "null-btns";
+    pageTwoNullbtn.type = "submit";
+    pageTwoNullbtn.name = "btn";
+    pageTwoNullbtn.value = "pageTwoNull";
+    var linebreak = document.createElement('br');
+    document.querySelector("#nullPagesDiv").appendChild(linebreak);
+    document.querySelector("#nullPagesDiv").appendChild(pageTwoNullbtn);
 } 
 
 if (pageThreeHasNull == "true") {
-    var pageThreeNullMessage = document.createElement('a');
-    pageThreeNullMessage.href = "./page_3";
-    pageThreeNullMessage.innerHTML = "Page Three";
-    document.querySelector("#reviewMessagePagesDiv").appendChild(pageThreeNullMessage);
-    document.querySelector('#reviewMessagePagesDiv').appendChild(buffer3);
+    var pageThreeNullbtn = document.createElement('button');
+    pageThreeNullbtn.innerHTML = "Page Three";
+    pageThreeNullbtn.className = "null-btns";
+    pageThreeNullbtn.type = "submit";
+    pageThreeNullbtn.name = "btn";
+    pageThreeNullbtn.value = "pageThreeNull";
+    var linebreak = document.createElement('br');
+    document.querySelector("#nullPagesDiv").appendChild(linebreak);
+    document.querySelector("#nullPagesDiv").appendChild(pageThreeNullbtn);
 } 
 
 if (pageFourHasNull == "true") {
-    var pageFourNullMessage = document.createElement('a');
-    pageFourNullMessage.href = "./page_4";
-    pageFourNullMessage.innerHTML = "Page Four";
-    document.querySelector("#reviewMessagePagesDiv").appendChild(pageFourNullMessage);
-    document.querySelector('#reviewMessagePagesDiv').appendChild(buffer4);
+    var pageFourNullbtn = document.createElement('button');
+    pageFourNullbtn.innerHTML = "Page Four";
+    pageFourNullbtn.className = "null-btns";
+    pageFourNullbtn.type = "submit";
+    pageFourNullbtn.name = "btn";
+    pageFourNullbtn.value = "pageFourNull";
+    var linebreak = document.createElement('br');
+    document.querySelector("#nullPagesDiv").appendChild(linebreak);
+    document.querySelector("#nullPagesDiv").appendChild(pageFourNullbtn);
 } 
 
 if (pageFiveHasNull == "true") {
-    var pageFiveNullMessage = document.createElement('a');
-    pageFiveNullMessage.href = "./page_5";
-    pageFiveNullMessage.innerHTML = "Page Five";
-    document.querySelector("#reviewMessagePagesDiv").appendChild(pageFiveNullMessage);
+    var pageFiveNullbtn = document.createElement('button');
+    pageFiveNullbtn.innerHTML = "Page Five";
+    pageFiveNullbtn.className = "null-btns";
+    pageFiveNullbtn.type = "submit";
+    pageFiveNullbtn.name = "btn";
+    pageFiveNullbtn.value = "pageFiveNull";
+    var linebreak = document.createElement('br');
+    document.querySelector("#nullPagesDiv").appendChild(linebreak);
+    document.querySelector("#nullPagesDiv").appendChild(pageFiveNullbtn);
 } 
 
-
+var nullBtns = document.querySelectorAll('.null-btns');
+var form = document.querySelector("#formDiv");
