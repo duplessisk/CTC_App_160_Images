229245dc8f2c7e61ac12666bb20bbe412a22b7ac
duplessisk <klduplessis@gmail.com>
2021-01-31 16:58:12 -0800

    create page one simplified

--- client_side_code/create_pages/create_page_1.js
+++ client_side_code/create_pages/create_page_1.js
@@ -1,36 +1,16 @@
-for (var i = 0; i < 10; i++) {
-    var newQuestion = document.createElement('div');
-    document.querySelector("#form").appendChild(newQuestion);
-    newQuestion.innerHTML = 
-        "<div class='new-image-row'> "+ 
-            "<div class='checkboxes-container'>" + 
-                "<div class='checkbox-container'>" + 
-                    "<label for='myRadioIdYes"+i+"' class='radio-btn-labels'>" + 
-                        "<input type='radio' value='yes' name='radio"+i+"' class='default-radio-btns yes_check_boxes' id='myRadioIdYes"+i+"'>" + 
-                        "<div class='custom-radio-btns'></div>" + 
-                        "Cell" + 
-                    "</label>" +
-                "</div>" + 
-                "<div id='buffer'></div>" + 
-                "<div class='checkbox-container'>" + 
-                "<label for='myRadioIdNo"+i+"' class='radio-btn-labels'>" + 
-                    "<input type='radio' value='no' name='radio"+i+"' class='default-radio-btns no_check_boxes' id='myRadioIdNo"+i+"'>" + 
-                    "<div class='custom-radio-btns'></div>" + 
-                    "Not Cell" + 
-                "</label>" +
-            "</div>" + 
-                "</div>" +
-                "<div class= 'cell-image-row-container'>" + 
-                    "<img src='/static/object_images/object0"+i+".png' alt='This image was originally intended to display a row of cell images' id='cellImage'>" + 
-                "</div>" + 
-            "</div>" + 
-        "</div>"
-}
+/**
+ * This script first creates the checkboxes, then creates the next button. Only when
+ * both of those elements are created is the page validated 
+ */
 
-var nextBtn = document.createElement('input');
-nextBtn.type = "submit";
-nextBtn.className = "btn";
-nextBtn.id = "nextBtn";
-nextBtn.name = "btn";
-nextBtn.value = "Next";
-document.querySelector("#form").appendChild(nextBtn);
+$.getScript('/static/create_pages/page_elements/checkboxes/create_checkboxes.js', function() {
+
+    $.getScript('/static/create_pages/page_elements/buttons/create_next_button.js', function() {
+        
+        var validatePageOne = document.createElement('script');
+        validatePageOne.src = "/static/validate_form_submissions/validate_form_submission_page_1.js";
+        document.head.appendChild(validatePageOne);
+    
+    });
+
+});
---
+++ client_side_code/create_pages/page_elements/checkboxes/create_checkboxes.js
@@ -0,0 +1,28 @@
+for (var i = 0; i < 10; i++) {
+    var newQuestion = document.createElement('div');
+    document.querySelector("#form").appendChild(newQuestion);
+    newQuestion.innerHTML = 
+        "<div class='new-image-row'> "+ 
+            "<div class='checkboxes-container'>" + 
+                "<div class='checkbox-container'>" + 
+                    "<label for='myRadioIdYes"+i+"' class='radio-btn-labels'>" + 
+                        "<input type='radio' value='yes' name='radio"+i+"' class='default-radio-btns yes_check_boxes' id='myRadioIdYes"+i+"'>" + 
+                        "<div class='custom-radio-btns'></div>" + 
+                        "Cell" + 
+                    "</label>" +
+                "</div>" + 
+                "<div id='buffer'></div>" + 
+                "<div class='checkbox-container'>" + 
+                "<label for='myRadioIdNo"+i+"' class='radio-btn-labels'>" + 
+                    "<input type='radio' value='no' name='radio"+i+"' class='default-radio-btns no_check_boxes' id='myRadioIdNo"+i+"'>" + 
+                    "<div class='custom-radio-btns'></div>" + 
+                    "Not Cell" + 
+                "</label>" +
+            "</div>" + 
+                "</div>" +
+                "<div class= 'cell-image-row-container'>" + 
+                    "<img src='/static/object_images/object0"+i+".png' alt='This image was originally intended to display a row of cell images' id='cellImage'>" + 
+                "</div>" + 
+            "</div>" + 
+        "</div>"
+}
